e2f21b61e041f7367e35e286e78c3ed70b2c55fa
SocksTheWolf <132639882+SocksTheWolf@users.noreply.github.com>
2026-07-28 00:24:59 -0700

    push it all

---
+++ README.md
@@ -0,0 +1,16 @@
+# Areen Killer
+
+This kills Areen. No one liked his fake ass retweeting bullshit anyways. Fuck that guy.
+
+This listens to a very specific PDS, and then adds all accounts from said PDS to the given user mute list.
+
+Run with `npm run watch`
+
+## CONFIG
+
+- `BSKY_USERNAME` - The username of who owns the mod list
+- `BSKY_PASSWORD` - The app password of who owns the mod list
+- `MOD_LIST` - The user list did that you want to add all accounts to.
+- `FIREHOSE_URL` - The websocket to listen to.
+
+I swear to god, I fucking hate this.
---
+++ main.js
@@ -0,0 +1,105 @@
+import { Agent, AppBskyGraphListRecord, CredentialSession } from '@atproto/api';
+import dotenv from "dotenv";
+import { Firehose } from "@skyware/firehose";
+
+// load the dotenv files
+dotenv.config({debug: false});
+
+const pdsToWatch = process.env.FIREHOSE_URL;
+
+// There's no real error handling here, I don't care.
+async function getBskyAgent() {
+  try
+  {
+    const session = new CredentialSession(new URL("https://bsky.social"));
+    await session.login({
+      identifier: process.env.BSKY_USERNAME,
+      password: process.env.BSKY_PASSWORD
+    });
+    return new Agent(session);
+  } catch(err) {
+    console.error(`Could not get bsky agent ${err}`);
+  }
+  return null;
+};
+
+// fuck you areen, die.
+async function addUserToList(agent, userDid) {
+  try {
+    await agent.com.atproto.repo.createRecord({
+      repo: agent.did,
+      collection: 'app.bsky.graph.listitem',
+      record: {
+        $type: 'app.bsky.graph.listitem',
+        subject: userDid,
+        list: process.env.MOD_LIST,
+        createdAt: new Date().toISOString()
+      }
+    });
+    console.log(`added ${userDid} to list`);
+    return true;
+  } catch(ex) {
+    console.error(ex);
+  }
+  return false;
+}
+
+// our bsky agent to manage adding to the list.
+let ourAgentToRainDown = null;
+const createBskyAgentNow = async () => {
+  if (ourAgentToRainDown == null) {
+    ourAgentToRainDown = await getBskyAgent();
+    if (ourAgentToRainDown !== null) {
+      console.log("Established bsky agent");
+    } else {
+      console.log("failed to establish bsky agent, will try again next loop");
+    }
+  }
+};
+
+// list of new areen accounts
+let didList = [];
+// process the list (do this after every message we receive)
+const processDidList = async () => {
+  const currentListSize = didList.length;
+  for (let count = 0; count < currentListSize; ++count) {
+    // if our agent became invalid, then recreate it before continuing
+    if (ourAgentToRainDown == null) {
+      await createBskyAgentNow();
+    }
+    // grab the first item in the list
+    const accountToRemove = didList.shift();
+    // check to see if we added to the list
+    const didAddAccountToList = await addUserToList(ourAgentToRainDown, accountToRemove);
+    // This is really bad but I don't really care, bad accounts get thrown in and tried again later
+    if (!didAddAccountToList) {
+      console.log("failed to add user, retrying again");
+      didList.push(accountToRemove);
+      ourAgentToRainDown = null;
+    }
+  }
+};
+
+// Listen to areen's bullshit pds. Note you are making a direct connection to their server
+// it's better for you to go through a proxy tbh
+const firehose = new Firehose({relay: pdsToWatch, autoReconnect: false});
+firehose.on("commit", async (message) => {
+  for (const op of message.ops) {
+    // skip everything that isn't a profile
+    if (!op.path.includes("app.bsky.actor.profile") || op.action != "create")
+      continue;
+
+    console.log(`GOT NEW RECORD ${op.record.displayName}`);
+    // we could probably do more to make sure this is a domain. like regex, but w/e
+    didList.push(`did:web:${op.record.displayName}`);
+  }
+  await processDidList();
+});
+firehose.on("open", () => {
+  console.log("connection established");
+});
+firehose.on("close", (cur) => {
+  console.log("connection was closed, starting again");
+  firehose.start();
+});
+firehose.start();
